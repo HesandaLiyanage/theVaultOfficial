17dfbf15881a3202d4539cbfbb931bdacd2abf27
HesandaLiyanage <nhesanda@gmail.com>
2026-04-30 16:53:53 +0530

    initial commit

---
+++ src/main/java/com/hess/thevault/TheVaultApplication.java
@@ -0,0 +1,13 @@
+package com.hess.thevault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheVaultApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hess/thevault/TheVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hess.thevault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
