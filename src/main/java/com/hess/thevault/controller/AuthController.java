c84c0f8437f8f3bd81f2dfda3f0239c9f1615006
HesandaLiyanage <nhesanda@gmail.com>
2026-05-01 11:06:23 +0530

    features

---
+++ src/main/java/com/hess/thevault/controller/AuthController.java
@@ -0,0 +1,11 @@
+package com.hess.thevault.controller;
+
+
+import com.hess.thevault.model.User;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+
+}
